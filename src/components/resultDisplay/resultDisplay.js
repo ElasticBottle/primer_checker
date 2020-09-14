d87c89c54b069d1bfffa5f2617e1fdef71ad0416
Winston <winstonyeo99@yahoo.com>
2020-09-14 15:19:21 +0800

    ✨ Catch invalid result navigation

--- src/components/resultDisplay/resultDisplay.js
+++ src/components/resultDisplay/resultDisplay.js
@@ -1,22 +1,30 @@
 import React from 'react';
 
-import Row from 'react-bootstrap/Row';
+import Container from 'react-bootstrap/Container';
+import Button from 'react-bootstrap/Button';
+import { useHistory } from 'react-router-dom';
 
 import OverviewDisplay from '../overviewDisplay/overviewDisplay';
 import PrimerDisplay from '../primerDisplay/primerDisplay';
 
 import './resultDisplay.css';
 
-const resultDisplay = ({ toDisplay, displayData, setToDisplay }) => {
+const InputData = () => {
+    const history = useHistory();
+    return (<Container>
+        <p className='no-data'>Please make sure that you've uploaded some data first!</p>
+        <Button onClick={() => history.push('/')}>Upload Data</Button>
+    </Container>);
+}
+
+const resultDisplay = ({ toDisplay, displayData }) => {
     return (<div className='result-display-container'>
-        <Row>
-            {displayData !== undefined ?
-                toDisplay === 'Overview' ?
-                    <OverviewDisplay data={displayData} />
-                    : <PrimerDisplay data={displayData} />
-                : null
-            }
-        </Row>
+        {displayData !== undefined ?
+            toDisplay === 'Overview' ?
+                <OverviewDisplay data={displayData} />
+                : <PrimerDisplay data={displayData} />
+            : <InputData />
+        }
     </div>);
 }
 
