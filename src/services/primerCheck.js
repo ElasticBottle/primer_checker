8b146e7e34a251b1ea6118538571c9c47bd1e56a
Winston <winstonyeo99@yahoo.com>
2020-09-23 17:53:53 +0800

    ➕ Updating dependencies for table plotting
    Removed unused dependencies as well.
    
    Update some formatting in index and primerCheck

--- src/services/primerCheck.js
+++ src/services/primerCheck.js
@@ -1,14 +1,16 @@
-import axios from 'axios'
+import axios from "axios";
 
-
-const baseURL = 'http://localhost:3001/persons'
+// const baseURL = 'https://mendel.bii.a-star.edu.sg/METHODS/corona/gamma/cgi-bin/primerMVP.py'
+const baseURL = "http://localhost:3001";
 
 const checkFile = (file) => {
-    return axios.post(`${baseURL}/check_primers`, file)
-}
+  return axios.post(`${baseURL}`, file);
+};
 
 const checkFiles = (files) => {
-    return axios.post(`${baseURL}/check_primers`, file)
-}
+  const to_send = { data: files };
+  // return axios.post(`${baseURL}`, to_send)
+  return axios.post(`${baseURL}/check_primers`, to_send);
+};
 
-export default { checkFile, checkFiles }
+export default { checkFile, checkFiles };
