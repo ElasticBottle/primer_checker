a052287c8447d83ad4f7dec093ebce946d5f88c4
Winston <winstonyeo99@yahoo.com>
2020-09-29 17:38:01 +0800

    🚀 Deploy test build

---
+++ build/precache-manifest.913fcec5e0808354df7b09b96b04b7bb.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "62cb0f37dfe62eb0e114d116b8c64e6c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f62b5da5ddfa64ddb69c",
+    "url": "/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "436fc29870a316320900",
+    "url": "/static/css/main.dc1b13f4.chunk.css"
+  },
+  {
+    "revision": "f62b5da5ddfa64ddb69c",
+    "url": "/static/js/2.3c2a3a45.chunk.js"
+  },
+  {
+    "revision": "27261f9594ae9c17415e985f83f339f8",
+    "url": "/static/js/2.3c2a3a45.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "436fc29870a316320900",
+    "url": "/static/js/main.9c1cec40.chunk.js"
+  },
+  {
+    "revision": "0ad21857adf5e19fd5db",
+    "url": "/static/js/runtime-main.12597e2e.js"
+  }
+]);
